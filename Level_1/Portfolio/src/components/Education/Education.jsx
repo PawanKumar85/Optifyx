438063d27421480e324c6190ce58dfe859f5a488
Pawan Kumar <pawan630703@gmail.com>
2024-09-22 01:56:51 +0530

    education section linked with backend

--- Level_1/Portfolio/src/components/Education/Education.jsx
+++ Level_1/Portfolio/src/components/Education/Education.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Education.css";
-const Education = ({ data }) => {
+import axios from "axios";
+
+const Education = () => {
+  const [educationData, setEducationData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/v2/portfolio/education"
+        );
+        setEducationData(response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <section className="education" id="education">
       <h2 className="heading">Education</h2>
       <div className="timeline-items">
-        {data.qualification.map((item, id) => {
-          return (
-            <div className="timeline-item" key={id}>
-              <div className="timeline-dot"></div>
-              <div className="timeline-date">{item.duration}</div>
-              <div className="timeline-content">
-                <h3>{item.title}</h3>
-                <span>
-                  {item.course} : {item.courseID}
-                </span>
-                <p>{item.collage}</p>
-              </div>
+        {educationData.map((item) => (
+          <div className="timeline-item" key={item._id}>
+            <div className="timeline-dot"></div>
+            <div className="timeline-date">{item.duration}</div>
+            <div className="timeline-content">
+              <h3>{item.title}</h3>
+              <span>
+                {item.course} : {item.courseID}
+              </span>
+              <p>{item.college}</p>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </section>
   );

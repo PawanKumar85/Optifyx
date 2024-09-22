6367d0497e2e0e2baeecf40f45d15ec14160d625
Pawan Kumar <pawan630703@gmail.com>
2024-09-22 16:34:47 +0530

    minor changes in app.jsx

--- Level_1/Portfolio/src/App.jsx
+++ Level_1/Portfolio/src/App.jsx
@@ -3,14 +3,12 @@ import style from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
-import Experience from "./components/Experience/Experience";
 import Platform from "./components/Platform/Platform";
 import Projects from "./components/Projects/Projects";
 import data from "./Data/personalData.json";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer";
-import Achievement from "./components/Achievement/Achievement";
 
 
 function App() {
@@ -18,13 +16,11 @@ function App() {
   return (
     <div className={style.App}>
       <Navbar data={data} />
-      <Hero data={data} />
-      <About data={data} />
-      <Education data={data}/>
-      <Experience data = {data} />
-      <Platform data={data}/>
-      <Projects data={data}/>
-      {/* <Achievement data={data}/> */}
+      <Hero/>
+      <About/>
+      <Education/>
+      <Platform/>
+      <Projects/>
       <Contact/>
       <Footer/>
 

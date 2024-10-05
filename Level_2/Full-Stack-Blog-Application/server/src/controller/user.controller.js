82a68c8b0fc2fd3f19d63952b98ad65bc5e9e413
Pawan Kumar <pawan630703@gmail.com>
2024-10-05 13:42:31 +0530

    log in successfully

--- Level_2/Full-Stack-Blog-Application/server/src/controller/user.controller.js
+++ Level_2/Full-Stack-Blog-Application/server/src/controller/user.controller.js
@@ -1,6 +1,24 @@
 import User from "../model/user.model.js";
 import emailValidator from "email-validator";
 
+const generateAccessAndRefereshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return {
+      accessToken,
+      refreshToken,
+    };
+  } catch (error) {
+    console.error("Error generating tokens:", error.message);
+    throw new Error("Token generation failed");
+  }
+};
+
 export const signUp = async (req, res) => {
   try {
     const { userName, fullName, email, password } = req.body;
@@ -40,7 +58,7 @@ export const signUp = async (req, res) => {
 
     return res.status(200).json({
       success: true,
-      message: "User registered successfully"
+      message: "User registered successfully",
     });
   } catch (error) {
     return res.status(500).json({
@@ -74,6 +92,100 @@ export const getAllUsers = async (req, res) => {
       success: false,
       error: error.message,
       message: "Error retrieving users",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { userName, email, password } = req.body;
+
+    if ((!email || !userName) && !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email or username and password are required",
+      });
+    }
+
+    const user = await User.findOne({
+      $or: [{ userName }, { email }],
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or username",
+      });
+    }
+
+    const isMatch = await user.isPasswordCorrect(password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefereshToken(
+      user._id
+    );
+
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json({
+        success: true,
+        message: "Logged in successfully",
+        user: loggedInUser,
+      });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+      message: "Error logging in",
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $unset: {
+          refreshToken: 1,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, {}, "User logged Out"));
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+      message: "Error logging out",
     });
   }
 };
--- Level_2/Full-Stack-Blog-Application/server/src/routes/user.routes.js
+++ Level_2/Full-Stack-Blog-Application/server/src/routes/user.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { signUp, getAllUsers } from "../controller/user.controller.js";
+import {
+  signUp,
+  getAllUsers,
+  login,
+  logout,
+} from "../controller/user.controller.js";
+// import { verifyJWT } from "../middleware/user.auth.js";
 
 const route = express.Router();
 
 route
   .post("/sign-up", signUp)
-  .get("/all-users", getAllUsers);
+  .get("/all-users", getAllUsers)
+  .post("/login", login)
+  .post("/logout", logout);
 
 export default route;
